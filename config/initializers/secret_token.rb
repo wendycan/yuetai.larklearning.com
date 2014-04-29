253b635a17efbfa985a28a8bde232d7fe35d5f3d
Wendy Wang <wilxnwang@gmail.com>
2014-04-29 09:41:34 +0800

    Create project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YuetaiWendycanOrg::Application.config.secret_key_base = '1ac45650c4a6b1884c9a3e1bd4fcf778cd7692c4d30b196a96313e53aa2ed80fafd8652ea6308b1ca2acd40dc689ef26fcb79a6d2e9d43383fcd773fe6f7457e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YuetaiWendycanOrg::Application.config.session_store :cookie_store, key: '_yuetai_wendycan_org_session'
