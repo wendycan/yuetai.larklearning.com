7e652897a20da47430f9c679af781ed7f33c39a1
Wendy Wang <wilxnwang@gmail.com>
2014-04-29 17:19:37 +0800

    Limit article index length, add html_safe

--- Gemfile
+++ Gemfile
@@ -15,7 +15,7 @@ gem 'uglifier', '>= 1.3.0'
 # Use CoffeeScript for .js.coffee assets and views
 gem 'coffee-rails', '~> 4.0.0'
 gem 'haml-rails'
-
+gem 'truncate_html', '0.3.2'
 # See https://github.com/sstephenson/execjs#readme for more supported runtimes
 # gem 'therubyracer', platforms: :ruby
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,9 @@
 
 Node.create([{name: 'music'},{name: 'life'},{name: 'others'},{name: 'book'}])
 Author.create([{name: 'wendywang'},{name: 'spacevisitor'},{name: 'unknow'}])
-Article.create([{name: "似水年华",uauthor: 'spacevisitor',utag: 'life', body: "<br/> 年华似水，每个人在这时光的灌溉下，懂得了些道理，形成了些习惯，收获了些快乐。某个时刻，我忽然又认识到，似水年华不经意地流走了二十几年，而最初梦想的种子还未破土而出，懒惰、无耐力的顽疾依然消磨着忙碌的身心。而这似水年华不仅是我的，也是你的，还是他的，每个人都在这时光的浪潮里航行着。浪淘沙，会发现什么，留下什么。
-      <br/> 即使在这似水年华中，个人渺小如浪潮中的一粒沙子，我们也可以发现更精彩的世界和自己，用生命刻下一些痕迹。
-      <br/> 在这里，我们记下些思绪，分享些知识。", music_script: "<embed src='http://www.xiami.com/widget/13261581_20526/singlePlayer.swf' type='application/x-shockwave-flash' width='257' height='33' wmode='transparent'></embed>
+Article.create([{name: "似水年华",uauthor: 'spacevisitor',utag: 'life', body: "<p>年华似水，每个人在这时光的灌溉下，
+  懂得了些道理，形成了些习惯,收获了些快乐。某个时刻，我忽然又认识到，似水年华不经意地流走了二十几年，而最初梦想的种子还未破土而出，
+  懒惰、无耐力的顽疾依然消磨着忙碌的身心。而这似水年华不仅是我的，也是你的，还是他的，每个人都在这时光的浪潮里航行着。浪淘沙，
+  会发现什么，留下什么。<br/>即使在这似水年华中，个人渺小如浪潮中的一粒沙子，我们也可以发现更精彩的世界和自己，用生命刻下一些痕迹。
+  <br/>在这里，我们记下些思绪，分享些知识。</p>", music_script: "<embed src='http://www.xiami.com/widget/13261581_20526/singlePlayer.swf' type='application/x-shockwave-flash' width='257' height='33' wmode='transparent'></embed>
       "}])
