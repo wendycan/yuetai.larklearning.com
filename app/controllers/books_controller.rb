fc52e2fe06893155f66f6e237633c263211fb501
Wendy Wang <wilxnwang@gmail.com>
2014-09-17 06:31:11 +0800

    Add lines

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -2,6 +2,7 @@ class BooksController < ApplicationController
   def index
     @books = Book.order("updated_at DESC").all
   end
+
   def show
     @book = Book.find_by_id(params[:id])
   end
--- app/controllers/chaps_controller.rb
+++ app/controllers/chaps_controller.rb
@@ -1,6 +1,7 @@
 class ChapsController < ApplicationController
   def index
   end
+
   def show
     @chap = Chap.find_by_id(params[:id])
   end
