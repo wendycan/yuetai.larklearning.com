fdf9492fd6c217e99738177d3edb4f5be6f11ead
Wendy Wang <wilxnwang@gmail.com>
2014-09-17 23:06:21 +0800

    Change javascript

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+//= require jquery
+//= require jquery_ujs
+//= require foundation
+//= require simple
+
+
+// This is for backbone mvc framework
+//
+//= require backbone
+//= require backbone/yuetai
+
+$(document).foundation();
