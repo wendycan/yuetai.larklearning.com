f34bb7a87537c37a4b5c6bc9a491ea5ea8efda9f
wendy <wendycaner@gmail.com>
2015-06-05 11:40:14 +0800

    Add articles all api

--- app/api/yuetai/articles_api.rb
+++ app/api/yuetai/articles_api.rb
@@ -2,6 +2,10 @@ module Yuetai
   class Articles < Grape::API
     resource :articles do
       desc 'Get all articles'
+      get do
+        blogs = Article.where(template: 'blog').order("created_at DESC").all
+        blogs
+      end
 
       route_param :id, requirements: /[^\/]+/ do
         get do
