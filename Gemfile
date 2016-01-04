93a9394735223fd57841dcb81068e280fff3900a
Wendy Wang <wilxnwang@gmail.com>
2016-01-04 20:42:34 +0800

    Add paginator

--- Gemfile
+++ Gemfile
@@ -42,6 +42,8 @@ gem 'turbolinks'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 gem 'jbuilder', '~> 1.2'
 
+gem 'will_paginate', '~> 3.0.6'
+
 group :doc do
   # bundle exec rake doc:rails generates the API under doc/api.
   gem 'sdoc', require: false
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-    @blogs = Article.order("updated_at DESC").where(template: 'blog')
+    @blogs = Article.order("updated_at DESC").where(template: 'blog').paginate(:page => params[:page], :per_page => 3)
+
   end
 
   def show
