4f0f4d271c6b73ef9228e96f60bc633bde140ecd
Wendy Wang <wilxnwang@gmail.com>
2016-01-05 20:26:40 +0800

    Add simditor

--- vendor/assets/javascripts/simple.js
+++ vendor/assets/javascripts/simple.js
@@ -1,1 +1,5 @@
 //= require alertify/alertify
+//= require simple-module/lib/module
+//= require simple-hotkeys/lib/hotkeys
+//= require simple-uploader/lib/uploader
+//= require simditor/lib/simditor
