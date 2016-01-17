dbbe5d01276cb9e669cafcdd4f9aeb189cf078d2
Wendy Wang <wilxnwang@gmail.com>
2016-01-17 15:24:06 +0800

    Add pager to user articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    @blogs = Article.order("created_at DESC").where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
+    @articles = Article.order("created_at DESC").where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
     @authors = User.all
   end
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find_by_id(params[:id])
-    @articles = @user.articles
+    @articles = @user.articles.order("created_at DESC").where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
   end
 
 end
