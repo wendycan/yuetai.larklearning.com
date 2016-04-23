fd4e392c1a905fcd3c91b5000e106385e4c29e02
wendy <wendy@wendys-MacBook-Air.local>
2016-04-23 10:55:00 +0800

    Style author lists

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
   def index
-    @articles = Article.order("created_at DESC").where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
-    @authors = User.all
+    @articles = Article.order('created_at DESC').where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
+    @authors = User.all.limit(3)
     @tags = Tag.all
   end
 
