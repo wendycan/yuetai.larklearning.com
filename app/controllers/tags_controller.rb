47046c73bb8cb51ffdcf95537103d27e57d18c84
wendycan <wendycaner@gmail.com>
2016-04-23 12:41:05 +0800

    Query tags and authors by articles length

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,8 +1,8 @@
 class TagsController < ApplicationController
   def show
     @tag = Tag.find_by_id(params[:id])
-    @tags = Tag.all
-    @articles = @tag.articles.order("created_at DESC")
+    @tags = Tag.joins('LEFT JOIN (select count(id) alength, tag_id from taggings a group by a.tag_id ) ac on ac.tag_id = tags.id').order('ac.alength desc')
+    @articles = @tag.articles.order('created_at DESC')
       .where(template: 'blog').paginate(:page => params[:page], :per_page => 10)
   end
 end
